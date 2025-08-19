2ca0f2c30046315dbf5d9984f4c68f99dcd2761a
Thxnxt <66070084@kmitl.ac.th>
2025-08-19 16:20:10 +0700

    w

--- app.py
+++ app.py
@@ -3,30 +3,38 @@ from flask import request
 from flask import render_template
 from flask import redirect
 from flask import url_for
+from pymongo import MongoClient
+from bson import ObjectId
+
+client = MongoClient("mongodb://mongo:27017/")
+mydb = client["ipa2025"]
+mycol = mydb["router"]
 
 sample = Flask(__name__)
 
 data = []
 
+
 @sample.route("/")
 def main():
-    return render_template("index.html", data=data)
+    return render_template("index.html", data=mycol.find({}))
 
 @sample.route("/add", methods=["POST"])
-def add_comment():
-    yourname = request.form.get("yourname")
-    message = request.form.get("message")
+def add_router():
+    ip = request.form.get("ip")
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-    if yourname and message:
-        data.append({"yourname": yourname, "message": message})
+    if ip and username and password:
+        mycol.insert_one({"ip": ip, "username": username, "password": password})
     return redirect(url_for("main"))
 
 @sample.route("/delete", methods=["POST"])
-def delete_comment():
+def delete_router():
     try:
-        idx = int(request.form.get("idx"))
-        if 0 <= idx < len(data):
-            data.pop(idx)
+        idx = request.form.get("idx")
+        if idx:
+            mycol.delete_one({"_id": ObjectId(idx)})
     except Exception:
         pass
     return redirect(url_for("main"))
